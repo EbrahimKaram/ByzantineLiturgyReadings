0a3a303fa6e97c1cd34d0f80a91cf163f50d425a
Bob_At_Work\Bob_On_Call <ebrahim_karam@hotmail.com>
2026-03-01 07:25:01 -0600

    Specified the port and added the referre urls

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,13 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),    VueDevTools()],
+  plugins: [vue(),
+  VueDevTools()],
+  server: {
+    port: 5173,      // Sets the dev server port
+    strictPort: true, // If 5173 is in use, Vite will fail instead of picking 5174
+  },
   // REPLACE 'repo-name' WITH YOUR ACTUAL GITHUB REPOSITORY NAME
   // Example: base: '/byzantine-readings/',
-  base: '/ByzantineLiturgyReadings/', 
+  base: '/ByzantineLiturgyReadings/',
 })
